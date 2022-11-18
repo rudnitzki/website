82610556e2b4d0df9b6c9d57cb8920ce950ba7ae
smohadjer <saeid@fastmail.fm>
2022-11-18 17:53:52 +0000

    Deploying to gh-pages from @ rudnitzki/website@9b8665e0220d7441bcbc7e69a1cab3881522fd2f 🚀

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,7 +6,6 @@
       throw new TypeError("Cannot call a class as a function");
     }
   }
-
   function _defineProperties(target, props) {
     for (var i = 0; i < props.length; i++) {
       var descriptor = props[i];
@@ -16,21 +15,21 @@
       Object.defineProperty(target, descriptor.key, descriptor);
     }
   }
-
   function _createClass(Constructor, protoProps, staticProps) {
     if (protoProps) _defineProperties(Constructor.prototype, protoProps);
     if (staticProps) _defineProperties(Constructor, staticProps);
+    Object.defineProperty(Constructor, "prototype", {
+      writable: false
+    });
     return Constructor;
   }
 
   var Navigation = /*#__PURE__*/function () {
     function Navigation(options) {
       _classCallCheck(this, Navigation);
-
       this.maxHeight = undefined;
       this.el = options.element;
       var self = this;
-
       if (this.el.getAttribute('data-set-max-height')) {
         this.el.style.maxHeight = 0;
         var observer = new MutationObserver(function () {
@@ -44,7 +43,6 @@
         });
       }
     }
-
     _createClass(Navigation, [{
       key: "getHeight",
       value: function getHeight() {
@@ -59,13 +57,12 @@
       key: "update",
       value: function update() {
         var self = this;
-
         if (this.el.classList.contains('is-visible')) {
           if (this.maxHeight === undefined) {
             this.maxHeight = this.getHeight();
-          } //wihtout setTimeout style change won't be animated
+          }
 
-
+          //wihtout setTimeout style change won't be animated
           setTimeout(function () {
             self.el.style.maxHeight = self.maxHeight + 'px';
           }, 0);
@@ -74,14 +71,15 @@
         }
       }
     }]);
-
     return Navigation;
   }();
 
+  //Helper classes to HTML for styling of nojs version
   var html = document.querySelector('html');
   html.classList.remove('no-js');
-  html.classList.add('js'); //taken from http://youmightnotneedjquery.com/
+  html.classList.add('js');
 
+  //taken from http://youmightnotneedjquery.com/
   function ready(fn) {
 
     if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
@@ -90,21 +88,20 @@
       document.addEventListener('DOMContentLoaded', fn);
     }
   }
-
   ready(function () {
 
-    console.log('DOM is ready!'); //initialize navigation
+    console.log('DOM is ready!');
 
+    //initialize navigation
     var nav = new Navigation({
       element: document.querySelector('header > nav')
-    }); //hamburger button
+    });
 
+    //hamburger button
     var hamburger = document.querySelector('button.hamburger');
-
     if (hamburger) {
       hamburger.addEventListener('click', function () {
         hamburger.classList.toggle('is-active');
-
         if (nav.el) {
           nav.el.classList.toggle('is-visible');
         }
